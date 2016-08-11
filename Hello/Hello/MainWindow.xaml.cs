8d8f7c86b8a9174ae9c0badca058134fb190ff62
Alex34467 <borisov.alexey0@gmail.com>
2016-08-11 18:51:21 +0300

    Create project.

---
+++ Hello/Hello/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Hello
+{
+    /// <summary>Code behind for MainWindow.xaml</summary>
+    public partial class MainWindow : Window
+    {
+        // Main window constructor.
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
